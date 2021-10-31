c49cce30f463731f849464e02f9022d68aeaba4b
Rob Fulwell <rob.fulwell@gmail.com>
2021-10-30 18:46:51 -0700

    refactor: introduce a simple Point class
    This also splits the nextpoint function into two parts.
    Also, unit tests! Yay!

--- sierpinski/main.py
+++ sierpinski/main.py
@@ -1,19 +1,40 @@
 import pygame
 import random
 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
-def pixel(surface, color, pos):
-    surface.fill(color, (pos, (1, 1)))
+# Arbitrarily chosen sizes that fit on my screen
+SIZE_X = 1000
+SIZE_Y = 1000
 
 
-def nextpoint(one, two, three, current):
+class Point:
+    x, y = 0, 0
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+def pixel(surface, color, pos: Point):
+    surface.fill(color, (pos.x, pos.y, 1, 1))
+
+
+def nextpoint(one: Point, two: Point, three: Point) -> Point:
     points = [one, two, three]
     random.shuffle(points)
-    target = points[0]
-    # return a position halfway between the current point and the target point
-    next_x = (current[0] + target[0]) / 2
-    next_y = (current[1] + target[1]) / 2
-    return (next_x, next_y)
+    return points[0]
+
+
+def halfway(point1: Point, point2: Point) -> Point:
+    next_x = (point1.x + point2.x) / 2
+    next_y = (point1.y + point2.y) / 2
+    return Point(int(next_x), int(next_y))
 
 
 def drawfps(screen, fps):
@@ -47,18 +68,11 @@ class Framerate:
             self.__index__ -= 1
 
 
-def init(screen, SIZE_X, SIZE_Y):
-    import itertools
-
-    INIT_POINT_COUNT = 4  # the three triangle vertices plus the initial start point
-    x_input = itertools.repeat(SIZE_X, INIT_POINT_COUNT)
-    y_input = itertools.repeat(SIZE_Y, INIT_POINT_COUNT)
-    one_x, two_x, three_x, start_x = map(random.randrange, x_input)
-    one_y, two_y, three_y, start_y = map(random.randrange, y_input)
-    point1 = (one_x, one_y)
-    point2 = (two_x, two_y)
-    point3 = (three_x, three_y)
-    start = (start_x, start_y)
+def init(screen: pygame.Surface, size_x: int, size_y: int):
+    point1 = Point(random.randrange(size_x), random.randrange(size_y))
+    point2 = Point(random.randrange(size_x), random.randrange(size_y))
+    point3 = Point(random.randrange(size_x), random.randrange(size_y))
+    start = Point(random.randrange(size_x), random.randrange(size_y))
 
     screen.fill(BLACK)
     pixel(screen, RED, point1)
@@ -69,57 +83,52 @@ def init(screen, SIZE_X, SIZE_Y):
     return point1, point2, point3, start
 
 
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-SIZE_X = 1000
-SIZE_Y = 1000
-
-pygame.init()
-size = (SIZE_X, SIZE_Y)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Sierpinski Demo")
-point1, point2, point3, current = init(screen, SIZE_X, SIZE_Y)
-pygame.display.flip()
-
-# Loop until the user clicks the close button.
-done = False
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-fps = Framerate()
-
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get():  # User did something
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                point1, point2, point3, current = init(screen, SIZE_X, SIZE_Y)
-                pygame.display.flip()
-            if event.key == pygame.K_KP_MINUS:
-                fps.down()
-            if event.key == pygame.K_KP_PLUS:
-                fps.up()
-            if event.key == pygame.K_q:
-                done = True
-        if event.type == pygame.QUIT:  # If user clicked close
-            done = True  # Flag that we are done so we exit this loop
-
-    # --- Game logic should go here
-    current = nextpoint(point1, point2, point3, current)
-
-    # --- Drawing code should go here
-    pixel(screen, WHITE, current)
-    drawfps(screen, fps)
-
-    # --- Go ahead and update the screen with what we've drawn.
+def main():
+    pygame.init()
+    size = (SIZE_X, SIZE_Y)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Sierpinski Demo")
+    point1, point2, point3, current = init(screen, SIZE_X, SIZE_Y)
     pygame.display.flip()
 
-    # --- Limit to user-defined frames per second
-    # this is a hack, I should be able to use `fps` directly here
-    clock.tick(int(f"{fps}"))
+    # Loop until the user clicks the close button.
+    done = False
+
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+    fps = Framerate()
+
+    # -------- Main Program Loop -----------
+    while not done:
+        # --- Main event loop
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    point1, point2, point3, current = init(screen, SIZE_X, SIZE_Y)
+                    pygame.display.flip()
+                if event.key == pygame.K_KP_MINUS:
+                    fps.down()
+                if event.key == pygame.K_KP_PLUS:
+                    fps.up()
+                if event.key == pygame.K_q:
+                    done = True
+            if event.type == pygame.QUIT:  # If user clicked close
+                done = True  # Flag that we are done so we exit this loop
+
+        # --- Game logic should go here
+        current = halfway(current, nextpoint(point1, point2, point3))
+
+        # --- Drawing code should go here
+        pixel(screen, WHITE, current)
+        drawfps(screen, fps)
+
+        # --- Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+
+        # --- Limit to user-defined frames per second
+        # this is a hack, I should be able to use `fps` directly here
+        clock.tick(int(f"{fps}"))
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_sierpinski.py
+++ tests/test_sierpinski.py
@@ -1,5 +1,39 @@
-from sierpinski import __version__
+import sierpinski
+import sierpinski.main
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert sierpinski.__version__ == "0.1.0"
+
+
+def test_nextpoint():
+    size_x, size_y = 1000, 1000
+    point1 = sierpinski.main.Point(size_x, size_y)
+    point2 = sierpinski.main.Point(size_x, size_y)
+    point3 = sierpinski.main.Point(size_x, size_y)
+    next = sierpinski.main.nextpoint(point1, point2, point3)
+    assert next in [point1, point2, point3]
+
+
+def test_halfway_square():
+    point1 = sierpinski.main.Point(0, 0)
+    point2 = sierpinski.main.Point(10, 10)
+    halfway = sierpinski.main.halfway(point1, point2)
+    assert halfway.x == 5
+    assert halfway.y == 5
+
+
+def test_halfway_x_axis():
+    point1 = sierpinski.main.Point(0, 0)
+    point2 = sierpinski.main.Point(10, 0)
+    halfway = sierpinski.main.halfway(point1, point2)
+    assert halfway.x == 5
+    assert halfway.y == 0
+
+
+def test_halfway_y_axis():
+    point1 = sierpinski.main.Point(0, 0)
+    point2 = sierpinski.main.Point(0, 10)
+    halfway = sierpinski.main.halfway(point1, point2)
+    assert halfway.x == 0
+    assert halfway.y == 5
